469b256b8427b09d230a4aa9d0fe7b89306e3bf3
huankong233 <huank0ng@foxmail.com>
2025-01-24 00:55:36 +0800

    [-] 移除无用依赖 [+] 增加进度条

--- src/renderer/src/utils/invoke.ts
+++ src/renderer/src/utils/invoke.ts
@@ -1,8 +1,11 @@
 import type { BaseResponse } from '@main/utils/response.ts'
 import { MessagePlugin } from 'tdesign-vue-next'
+import NProgress from '@renderer/utils/progress.ts'
 
 export const invoke = async <T = void>(method: string, data = {}) => {
+  NProgress.start()
   const res = (await window.ElectronAPI.ipcRenderer.invoke(method, data)) as BaseResponse<T>
+  NProgress.done()
   if (res.code === 200) {
     return res.data
   } else {
